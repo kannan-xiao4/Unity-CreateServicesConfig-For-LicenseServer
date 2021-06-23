6906b9afb599c48fa6181d2cd94a9543c8495eca
Takashi KANNAN <wingrakhi@live.jp>
2021-06-23 12:06:00 +0900

    install fetch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const wait = require('./wait');
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 // most @actions toolkit packages have async methods
 async function run() {
